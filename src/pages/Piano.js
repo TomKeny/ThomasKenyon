7db237184204a4e9097c99f27bd7e6bb0fdebc3b
Thomas Kenyon <tomkenyon2003@gmail.com>
2025-03-03 13:30:03 +0000

    added some templates for projects

--- src/pages/Piano.js
+++ src/pages/Piano.js
@@ -1,9 +1,31 @@
+import lin from "../assets/Lin.png"
+import insta from "../assets/insta.png"
+import github from "../assets/github.png"
+
 function Piano () {
-    return (<div>
-        <p>Piano</p>
-        <a href="/">home</a>
+    return (
+        <div id="siteContainer">
+            <div id="header">
+                <h1 id="mainTitle">Piano Soundboard</h1>
+                <a id="backButton" href="/">Home</a>
+            </div>
+            <div id="headerGradient" />
+    
+    
+            <div id="footer">
+                <div id="links">
+                    <a className='social' style={{backgroundImage: `url(${lin})`}} href="https://www.linkedin.com/in/thomaspkenyon/" target="blank"/>
+                    <a className='social' style={{backgroundImage: `url(${github})`}} href="https://github.com/TomKeny" target="_blank"/>
+                    <a className='social' style={{backgroundImage: `url(${insta})`}} href="https://www.instagram.com/frog.inapond/?next=%2Ffrog.inapond%2F" target="_blank" />
+                </div>
+                <p id="footerText">Email: tomkenyon2003@gmail.com</p>
+                <div id="footerBottom">
+                    <p id="copyright">© 2025 Thomas Kenyon</p>
+                </div>
+            </div>
+    
         </div>
-    )
+        )
 }
 
 export default Piano
